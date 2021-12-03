3d373834dabbb738ffd34649b2177392e40e85a3
yooonnn <>
2021-12-03 18:16:12 +0900

    21-12-03 타임어택

--- 211203/src/main/java/com/sparta/timeattack/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.timeattack.repository;
-
-import com.sparta.timeattack.domain.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
--- 211203/src/main/java/com/sparta/timeattack/repository/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.sparta.timeattack.repository;
-
-import com.sparta.timeattack.domain.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long> {
-}
